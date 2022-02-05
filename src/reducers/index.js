8c06ddbfee71f9abe40a7b252353b9bb07307d74
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2022-02-04 22:16:30 -0600

    final v2.0

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 
-import ui from './ui'
 import { fetchStatus, fetchErrors } from './fetchStatus'
 
 import user from './user'
 import profiles from './profiles'
 
 export default combineReducers({
-	ui,
 	fetchStatus,
 	fetchErrors,
 	
